9913b9da5f80bf5dc7fe26ea8178fecb0c19e513
JohnCalen <vivaleute@gmail.com>
2017-09-28 02:01:05 +0300

    Adding print method

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main
 {
 	public static void main(String sqcString[])
 	{
-		
+		print(sqcString);
+	}
+
+	public static void print(String sqcString[])
+	{
+		if (sqcString == null)
+			return;
+
+		for (int itera = 0; itera < sqcString.length; ++itera)
+			System.out.println(sqcString[itera]);
 	}
 }
