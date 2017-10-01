934f34ee230b21a036697c4263c08cc0e046c85c
JohnCalen <vivaleute@gmail.com>
2017-10-01 23:16:09 +0300

    Fix2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main
 {
 	public static void main(String sqcString[])
 	{
-		print(sqcString);
 		sort(sqcString);
+		print(sqcString);
 	}
 
 	public static void print(String sqcString[])
@@ -14,6 +14,26 @@ public class Main
 
 	public static void sort(String sqcString[])
 	{
-		print(_QuickSort.sort(sqcString));
+		String temp;
+		int j;
+		int length = sqcString.length;
+		for(int i = 0; i < length - 1; i++)
+		{
+			if (sqcString[i].compareTo(sqcString[i + 1]) > 0)
+			{
+				temp = sqcString[i + 1];
+				sqcString[i + 1] = sqcString[i];
+
+				j = i;
+
+				while (j > 0 && temp.compareTo(sqcString[j - 1]) < 0)
+				{
+					sqcString[j] = sqcString[j - 1];
+					j--;
+				}
+
+				sqcString[j] = temp;
+			}
+		}
 	}
 }
